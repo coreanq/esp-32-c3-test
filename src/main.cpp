b98f4e7c606abda1c07bd336124ddfaae1aa36a7
ls charles <coreanq@gmail.com>
2025-01-14 14:24:17 +0900

    keypad <-> 485 convertor <-> esp32 rx,tx 완성

--- src/main.cpp
+++ src/main.cpp
@@ -21,77 +21,49 @@
 const char *ssid = "yourAP";
 const char *password = "yourPassword";
 
-WiFiServer server(80);
 
+
+void printHexString(const char* str, uint32_t length);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
+  // Init Debug Serial
   Serial.begin(921600);
   Serial.println();
-  Serial.println("Configuring access point...");
-
-  // You can remove the password parameter if you want the AP to be open.
-  // a valid password must have more than 7 characters
-  if (!WiFi.softAP(ssid, password)) {
-    log_e("Soft AP creation failed.");
-    while(1);
-  }
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  server.begin();
-
-  Serial.println("Server started");
+  Serial.println("Debug Serial Init");
+  
+ 
+  // Init Comm Serial gpio 20 rx, gpio 21 tx
+  Serial1.begin(460800, SERIAL_8N1, 20, 21);
+  Serial.println("Comm Serial Init");
+  
+  // Set gpio10 to output
+  // 485 enable so set low to Receive mode
+  pinMode(10, OUTPUT);
+  digitalWrite(10, LOW);
+  
 }
 
+char buffer[256];
+
 void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
+  
+  uint32_t bytesRead = Serial1.readBytes(buffer, 256);
 
-  if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
+  if( bytesRead > 0 ) {
+    printHexString(buffer, bytesRead);
   }
+
+}
+
+void printHexString(const char* str, uint32_t length) {
+
+  for(uint32_t i = 0; i < length; i++) {
+    char buffer[3]; // 각 문자를 16진수로 변환하는 버퍼 (2자리 + null 문자)
+    sprintf(buffer, "%02X", (unsigned char)*str);
+    Serial.print(buffer);
+    str++;
+  }
+  Serial.println(); // 줄 바꿈
 }
